d4983635032df19eb6ba81f6b0643d1c6d36e472
valeriö <valeriodambrosio.14@gmail.com>
2020-05-29 16:32:45 +0200

    Vaglio i dati dell'array ...al contrario

--- palindroma/myscript.js
+++ palindroma/myscript.js
@@ -3,10 +3,20 @@ var parola = prompt("Scrivi una parola")
 
 //prendo il valore inserito e lo splitto in un array
 parola = parola.split("");
-console.log(parola)
+console.log(parola);
 
 //creo un ciclo per vagliare ogni singolo dato dell'array
+for (var i = 0; i < parola.length; i++) {
+
+  console.log(parola[i]);
+
+}
 
 //se il ciclo contrario è identico la parola è palindroma
+for (var j = parola.length; j >= 0; j--) {
+
+  console.log(parola[j]);
+
+}
 
 //stampo affermazione/negazione che sia palindroma
