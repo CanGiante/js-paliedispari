cfa04f08eccfa963043a47fe453af972d7d0f23c
valeriö <valeriodambrosio.14@gmail.com>
2020-05-29 15:54:15 +0200

    Aggiunti file base

---
+++ palindroma/myscript.js
@@ -0,0 +1,12 @@
+//chiedo di inserire una parola
+var parola = prompt("Scrivi una parola")
+
+//prendo il valore inserito e lo splitto in un array
+parola = parola.split("");
+console.log(parola)
+
+//creo un ciclo per vagliare ogni singolo dato dell'array
+
+//se il ciclo contrario è identico la parola è palindroma
+
+//stampo affermazione/negazione che sia palindroma
