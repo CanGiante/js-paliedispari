2df7b430b29c793bc7a538d5e1589af29df23562
valeriö <valeriodambrosio.14@gmail.com>
2020-05-31 18:21:46 +0200

    Fine primo esercizio

--- palindroma/myscript.js
+++ palindroma/myscript.js
@@ -1,22 +1,65 @@
-//chiedo di inserire una parola
-var parola = prompt("Scrivi una parola")
+//FUNZIONE PER VERIFICARE SE UNA PAROLA è PALINDROMA
+function verificaParolaPalindroma(parola, isPalindroma) {
 
-//prendo il valore inserito e lo splitto in un array
-parola = parola.split("");
-console.log(parola);
+  console.log("verifico se " + parola + " è una parola palindroma...");
 
-//creo un ciclo per vagliare ogni singolo dato dell'array
-for (var i = 0; i < parola.length; i++) {
+  isPalindroma = true;
 
-  console.log(parola[i]);
+  for (var i = 0; i < parola.length; i++) {
+
+    if (parola[i] != parola[parola.length - 1 - i]) {
+
+      isPalindroma = false;
+
+    }
+
+    console.log( "lettera num. " + (i + 1) );
+    console.log( (parola[i]) );
+    console.log( "lettera num. " + (parola.length - i) );
+    console.log( (parola[parola.length - 1 - i]) );
+    console.log(parola[i] === parola[parola.length - 1 - i]);
+
+  }
+
+  if (isPalindroma) {
+
+    console.log(parola + " è una parola palindroma.");
+
+  } else if (!isPalindroma) {
+
+    console.log(parola + " NON è una parola palindroma.");
+
+  }
+
+  return isPalindroma;
+
+}
+//FINE FUNZIONE
+
+
+////Test funzione
+var parolaInserita = prompt("Scrivi una parola");
+var test = verificaParolaPalindroma(parolaInserita);
+
+if (test) {
+
+  alert("la parola è palindroma");
+
+} else {
+
+  alert("la parola non è palindroma");
 
 }
 
-//se il ciclo contrario è identico la parola è palindroma
-for (var j = parola.length; j >= 0; j--) {
 
-  console.log(parola[j]);
-
-}
-
-//stampo affermazione/negazione che sia palindroma
+// var test = verificaParolaPalindroma("eletazzineigienizzatele");
+//
+// if (test) {
+//
+//   alert("la parola è palindroma");
+//
+// } else {
+//
+//   alert("la parola non è palindroma");
+//
+// }
