3cb1acf0526910582123faab7d167275472fa097
valeriö <valeriodambrosio.14@gmail.com>
2020-06-01 16:29:11 +0200

    Debug

--- pari e dispari/myscript.js
+++ pari e dispari/myscript.js
@@ -1,30 +1,12 @@
-/*FUNZIONE PER STABILIRE SE LA SOMMA FRA IL NUMERO GENERATO DAL PC E IL NUMERO INSERITO DALL'UTENTE è PARI*/
-function sommaPari(numUtente) {
-
-  var numRandomPc = Math.floor(Math.random() * (5)) + 1;
-  console.log("il computer sceglie " + numRandomPc);
-
-  var somma = numUtente + numRandomPc;
-  console.log("la somma è " + somma);
-
-  var isPari = somma % 2 === 0;
-  console.log("la somma è pari = " + isPari);
-
-  return isPari;
-
-}
-//FINE FUNZIONE
-
-
 //GIOCO "PARI E DISPARI"
 var pariODispari = prompt("pari o dispari?");
 console.log("hai scelto " + pariODispari);
-var sceltaNumero = parseInt(prompt("Inserisci un numero da 1 a 5"));
-console.log("hai scelto " + sceltaNumero);
+var numScelto = parseInt(prompt("Inserisci un numero da 1 a 5"));
+console.log("hai scelto " + numScelto);
 
-var numeroPari = sommaPari(sceltaNumero, pariODispari);
+var numeroPari = sommaPari(numScelto);
 
-if (sceltaNumero < 1 || sceltaNumero > 5) {
+if (numScelto < 1 || numScelto > 5) {
 
   alert("error 420");
 
@@ -43,3 +25,39 @@ if (sceltaNumero < 1 || sceltaNumero > 5) {
   }
 
 }
+
+
+
+//FUNZIONE PER STABILIRE SE LA SOMMA FRA IL NUMERO GENERATO DAL PC E IL NUMERO INSERITO DALL'UTENTE è PARI O DISPARI
+//ovvero per stabilire se isPari è true
+function sommaPari(numUtente) {
+
+  var numRandomPc = Math.floor(Math.random() * (5)) + 1;
+  console.log("il computer sceglie " + numRandomPc);
+
+  var somma = numUtente + numRandomPc;
+  console.log("la somma è " + somma);
+
+  var isPari = somma % 2 === 0;
+
+  if (isPari) {
+
+    console.log("la somma è pari");
+
+  } else {
+
+    console.log("la somma è dispari");
+
+  }
+
+
+  return isPari;
+
+}
+
+// //inizio debug funzione
+// var addendo = parseInt(prompt("Inserisci un numero da 1 a 5"));
+// console.log("hai scelto " + addendo);
+// sommaPari(addendo1);
+// //fine debug funzione
+//FINE FUNZIONE
